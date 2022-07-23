4d23963e3807648e9155ecac33edb51393e77cd7
ShimpeiHashizume <69233878+ShimpeiHashizume@users.noreply.github.com>
2022-07-23 16:32:33 +0900

    Initial commit

---
+++ src/components/StyledComponents.jsx
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+
+export const StyledComponents = () => {
+  return (
+    <SConteiner>
+      <STitle>- styled components -</STitle>
+      <SButton>FIGHT!!</SButton>
+    </SConteiner>
+  );
+};
+
+const SConteiner = styled.div`
+  border: solid 2px #392eff;
+  border-radius: 20px;
+  padding: 8px;
+  margin: 8px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+const STitle = styled.p`
+  margin: 0;
+  color: #3d84a8;
+`;
+const SButton = styled.button`
+background-color: #abedd8;
+  border: none;
+  padding: 8px;
+  border-radius: 8px;
+  &:hover {
+    background-color: #46cdcf;
+    color: #fff;
+    cursor: pointer;
+`;
